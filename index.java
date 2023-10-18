158e0837d28bb91dedf48eceede3988a87b97aa8
GavinDaffa <gavinsantoso02@gmail.com>
2023-10-18 11:37:10 +0700

    catatan github

--- index.java
+++ index.java
@@ -1,5 +1,19 @@
 public class index {
-    // welcome ma bro
+
     // nama saya gavin daffa
+
+    /*
+    Tutorial :
+    1. git config global user.email "email@gmail.com"
+    2. git config global user.name "username"
+    3. masuk direktori repositori folder
+    4. git init
+    git remote add origin url_github
+    5. git commit -m "komentar comitnya"
+    . semisal belum bisa bersihkan dulu
+    . get branch --uset-upstream
+    6. git branch -m main
+    7. git push origin main
+     */
     
 }
